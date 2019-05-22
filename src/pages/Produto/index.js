433ca9b4c69af07d394cbb423df04f4ddbefc168
markusnt <markus_nathan@live.com>
2019-05-22 15:38:56 -0300

    test

--- src/pages/Produto/index.js
+++ src/pages/Produto/index.js
@@ -1,40 +1,69 @@
-import React, { Component } from 'react';
-import api from '~/services/api';
+import React, { Component } from 'react'
 
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar, View,
+  Text,
+  View,
+  FlatList,
+  ImageBackground,
+  StyleSheet,
 } from 'react-native';
+
+import api from '~/services/api';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+});
 
 export default class Produto extends Component {
   static navigationOptions = {
-    title: 'Produto',
+    headerTransparent: true,
+    title: 'Produtos',
   };
 
-  state = {
-        produtos: []
+    state = {
+      produtos: [],
+    };
+
+    componentDidMount() {
+      this.getProdutosApi();
     }
 
-  componentDidMount() {
-    this.loadProdutos();
-  }
 
-  loadProdutos = async () => {
-    return await fetch(`${api}/produtoS/19`)
-    .then((response) => response.json())
-    .then((responseJson) => {
-
+  getProdutosApi = async () => {
+    return fetch('http://192.168.1.179:1337/produtoS/19') //cd_subgrupo
+      .then(response => response.json())
+      .then((responseJson) => {
         this.setState({
-            produtos: responseJson,
-        })
-    })
+          produtos: responseJson,
+        });
+      });
   }
+
+  renderProduto = ({ item }) => (
+    <View>
+      <Text>{item.ds_produto}  R${item.pr_produto.toFixed(2)}</Text>
+    </View>
+  )
 
   render() {
     return (
-      <View>
-        <Text> Brabo </Text>
-
-      </View>
+      <ImageBackground
+        source={{
+          uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
+        }}
+        style={styles.container}
+        resizeMode="cover"
+      >
+        <FlatList
+          data={this.state.produtos}
+          keyExtractor={({ id }, index) => `id${index}`}
+          renderItem={this.renderProduto}
+        />
+      </ImageBackground>
     );
   }
 }
